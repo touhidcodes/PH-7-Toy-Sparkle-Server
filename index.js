5c983f54c6a3a139be991132f788aa4627730d07
Touhidur Zaman <touhidcodes@gmail.com>
2023-05-19 01:49:28 +0600

    get-api-individual-data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -29,6 +29,14 @@ async function run() {
 		// get all data
 		app.get("/toys", async (req, res) => {
 			const result = await toyCollection.find().toArray();
+			res.send(result);
+		});
+
+		// get individual data
+		app.get("/details/:id", async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
+			const result = await toyCollection.findOne(query);
 			res.send(result);
 		});
 
