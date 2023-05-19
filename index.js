932c8dbe192fb21a8dffb2c9b742518b2a32b120
Touhidur Zaman <touhidcodes@gmail.com>
2023-05-19 22:40:40 +0600

    post-api-data

--- index.js
+++ index.js
@@ -40,6 +40,13 @@ async function run() {
 			res.send(result);
 		});
 
+		// post toy data
+		app.post("/add", async (req, res) => {
+			const toy = req.body;
+			const result = await toyCollection.insertOne(toy);
+			res.send(result);
+		});
+
 		// Send a ping to confirm a successful connection
 		await client.db("admin").command({ ping: 1 });
 		console.log(
